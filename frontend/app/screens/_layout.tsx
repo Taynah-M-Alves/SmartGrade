43e4eb421d0faa0e213ba1577659f8e3c9a67643
Taynah-M-Alves <taynahdemichelialves@gmail.com>
2026-05-30 17:16:49 -0400

    criando as rotas das telas

---
+++ frontend/app/screens/_layout.tsx
@@ -0,0 +1,34 @@
+import { Stack } from "expo-router";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function ScreensLayout() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>TESTANDO !!</Text>
+
+      <View style={styles.content}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#111827",
+  },
+
+  text: {
+    color: "#FFFFFF",
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 60,
+    marginBottom: 20,
+  },
+
+  content: {
+    flex: 1,
+  },
+});
