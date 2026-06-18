71fd2bd42f0df25c004b3fa462fae96774c6dda2
Taynah-M-Alves <taynahdemichelialves@gmail.com>
2026-06-17 22:01:08 -0400

    fix: remover babel.config.js, usar metro extraNodeModules para path aliases

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,5 +1,12 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver.extraNodeModules = {
+  hooks: path.resolve(__dirname, 'hooks'),
+  app: path.resolve(__dirname, 'app'),
+  assets: path.resolve(__dirname, 'assets'),
+};
 
 module.exports = config;
